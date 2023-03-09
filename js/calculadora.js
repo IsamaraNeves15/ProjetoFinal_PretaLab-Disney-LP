62092586154d2bdcff72effacd54b3a260ac1091
“isamaraneves15” <isamaraneves15@gmail.com>
2023-03-09 12:59:58 -0300

    calculadora atualizada

--- js/calculadora.js
+++ js/calculadora.js
@@ -3,13 +3,13 @@ function somar() {
   const numero2 = parseFloat(document.getElementById("numero2Soma").value);
   // evite mexer no código acima!
   
-  const resultado = numero1 + numero2;
+  const soma = numero1 + numero2;
 
 
 
   /* use as constantes numero 1 e numero 2 e realize a soma entre elas.
   Em seguida, ao invés de mostrar o numero 10, mostre o resultado da operação */
-  document.getElementById("resultadoSoma").innerHTML = resultado;
+  document.getElementById("resultadoSoma").innerHTML = soma;
 }
 
 function subtrair() {
@@ -17,13 +17,13 @@ function subtrair() {
   const numero2 = parseFloat(document.getElementById("numero2Sub").value);
   // evite mexer no código acima!
 
-
+const subtracao = numero1 - numero2;
 
 
 
   /* use as constantes numero 1 e numero 2 e realize a soma entre elas.
    Em seguida, ao invés de mostrar o numero 9, mostre o resultado da operação */
-  document.getElementById("resultadoSub").innerHTML = 9;
+  document.getElementById("resultadoSub").innerHTML = subtracao;
 }
 
 function multiplicar() {
@@ -32,12 +32,12 @@ function multiplicar() {
   // evite mexer no código acima!
 
 
-
+const multiplicacao = numero1 * numero2;
 
 
   /* use as constantes numero 1 e numero 2 e realize a soma entre elas.
    Em seguida, ao invés de mostrar o numero 5, mostre o resultado da operação */
-  document.getElementById("resultadoMult").innerHTML = 5;
+  document.getElementById("resultadoMult").innerHTML = multiplicacao;
 }
 
 function dividir() {
@@ -46,10 +46,10 @@ function dividir() {
   // evite mexer no código acima!
 
 
-
+const divisao = numero1 / numero2;
 
   
   /* use as constantes numero 1 e numero 2 e realize a soma entre elas.
    Em seguida, ao invés de mostrar o numero -89, mostre o resultado da operação */
-  document.getElementById("resultadoDiv").innerHTML = -89;
+  document.getElementById("resultadoDiv").innerHTML = divisao;
 }
